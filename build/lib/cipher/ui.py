cd9aa7dcd41924f959750882bb78ef395990ab33
Programming geek <htmlgeek752@gmail.com>
2022-08-12 19:12:14 +0530

    Finally

---
+++ build/lib/cipher/ui.py
@@ -0,0 +1,17 @@
+from click import secho
+from . import __author__, __copyright__, __description__
+
+def initialise_cipher(private_key):
+	secho(f"""
+  ____ _       _
+ / ___(_)_ __ | |__   ___ _ __
+| |   | | '_ \| '_ \ / _ \ '__|		{__description__}
+| |___| | |_) | | | |  __/ |		
+ \____|_| .__/|_| |_|\___|_|		{__copyright__}
+        |_|
+		""",
+		fg="bright_red"
+	)
+	print(f"""\nThe private key is saved in the file called key 
+so pls make sure to keep this file safe and also
+Not to make any change to the file content or name.\n""")
